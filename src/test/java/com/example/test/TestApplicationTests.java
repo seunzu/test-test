edcefbced6999d92dbd4b412e1ed19f592012834
seunzu <0123suh@gmail.com>
2024-11-29 20:24:47 +0900

    first commit

---
+++ src/test/java/com/example/test/TestApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertThat(1).isEqualTo(1+1);
+    }
+
+}
